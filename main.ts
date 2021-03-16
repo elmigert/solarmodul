dae50c4a88eba30e970b5cabe2ec5f9aab940a6d
elmigert <elmigert@student.ethz.ch>
2021-03-16 10:48:19 +0100

    Aktualisiere main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,17 @@
+let loading = false;
+
 basic.forever(function on_forever() {
 
 
 if (input.buttonIsPressed(Button.A)){
-
+ loadBattery();
 }
 
 })
 
 function loadBattery(){
+
     
+    pins.analogWritePin(AnalogPin.P0, 1023)
 }
 
